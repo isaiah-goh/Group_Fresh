1af0196da98552126e71788817b7e42889a8e5f2
Yingweii <109503995+Yingweii@users.noreply.github.com>
2022-07-22 12:51:43 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ import requests
 api_key = "demo"
 url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_symbol=USD&to_symbol=SGD&apikey={api_key}"
 response = requests.get(url)
-print(response)
+currency_exchange_rate = response.json()
