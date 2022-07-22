96704901ed0c17f9a4975d8ba8537649ad54b9ad
Yingweii <109503995+Yingweii@users.noreply.github.com>
2022-07-22 12:57:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import csv
 import requests
 
-api_key = "demo"
+api_key = "0GB2H0EPEH2W014M"
 url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_symbol=USD&to_symbol=SGD&apikey={api_key}"
 response = requests.get(url)
 currency_exchange_rate = response.json()
