f7d2814b923280cb86a811759c9449dfa191c285
Yingweii <109503995+Yingweii@users.noreply.github.com>
2022-08-02 15:37:34 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,51 +6,48 @@ url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_c
 response = requests.get(url)
 currency_exchange_rate = response.json()
 
-def forex():
-    exchange_rate = []
-    for items in currency_exchange_rate:
-        exchange_rate.append(currency_exchange_rate[items]['5. Exchange Rate'])
-    print(f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{exchange_rate[0]}")
-forex()
 
+exchange_rate = []
+for items in currency_exchange_rate:
+    exchange_rate.append(currency_exchange_rate[items]['5. Exchange Rate'])
 
 
 #profit_and_loss
 from pathlib import Path
-import csv
+import csv, api
 
 
 empty_list=[]
 date=[]
-fp=Path.cwd()/"profit-and-loss-usd.csv"
+fp=Path.cwd()/"csv_reports"/"Profits and Loss.csv"
 with fp.open(mode="r",encoding="UTF-8")as file:
     reader=csv.reader(file)
     next(reader)
     for values in reader:
         empty_list.append(values[4])
         date.append(values[0])
+
 def profit_and_loss():
     number_1=-1
     number_2=0       
     difference=[]
-    non_deficits=[]
     for items in range(0,5):
         number_1+=1
         number_2+=1
         difference.append((int(empty_list[number_2])-int(empty_list[number_1])))
     for date,subtract in enumerate(difference,37):
         if subtract<0:
-            print(f"[CASH DEFICIT] DAY {date}, AMOUNT :SGD{subtract*-1}")
+            value = subtract* -1
+            print(f"[PROFIT DEFICIT] DAY {date}, AMOUNT :SGD{float(value)*float(api.exchange_rate[0])}")
 profit_and_loss()
-
         
 #Cash on Hand
 from pathlib import Path
-import csv
+import csv, api
 
 amount = []
 date = []
-fp = Path.cwd()/"Cash_on_hand"/"cash.csv"
+fp = Path.cwd()/"csv_reports"/"Cash on Hand.csv"
 with fp.open(mode ="r", encoding="UTF-8") as x:
     reader=csv.reader(x)
     next(reader)
@@ -68,18 +65,18 @@ def coh():
         difference.append((int(amount[number_2])-int(amount[number_1])))
     for date,subtract in enumerate(difference,37):
         if subtract<0:
-            print(f"[CASH DEFICIT] DAY {date}, AMOUNT :SGD{subtract*-1}")
+            value = subtract* -1
+            print(f"[CASH DEFICIT] DAY {date}, AMOUNT :SGD{float(value)*float(api.exchange_rate[0])}")
 coh()
-    
 
 #OVERHEADS 
 from pathlib import Path
-import csv
+import csv, api
 
 expenses_value=[]
 expenses_type=[]
 
-fp = Path.cwd()/"overheads_day_41.csv"
+fp = Path.cwd()/"csv_reports"/"Overheads.csv"
 
 with fp.open(mode="r", encoding="UTF-8") as file:
     reader = csv.reader(file)
@@ -87,9 +84,9 @@ with fp.open(mode="r", encoding="UTF-8") as file:
     for values in reader:
         expenses_type.append(values[0])
         expenses_value.append(values[1])
-        
+
 def expense():
     for i,amount in enumerate(expenses_value):
         if amount == max(expenses_value):
-            print(f"[HIGHEST OVERHEADS] {expenses_type[i]}: SGD{amount}")
+            print(f"[HIGHEST OVERHEADS] {expenses_type[i]}: SGD{float(amount)*float(api.exchange_rate[0])}")
 expense()
